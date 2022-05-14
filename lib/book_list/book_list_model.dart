df595909ef37bb4eddcb41c84097673a0040ae15
‘terupro’ <terupro911@gmail.com>
2022-05-14 15:00:02 +0900

    Firebaseの値を1回だけ取得できるように変更(getメソッド)

--- lib/book_list/book_list_model.dart
+++ lib/book_list/book_list_model.dart
@@ -3,21 +3,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class BookListModel extends ChangeNotifier {
-  final Stream<QuerySnapshot> _stream =
-      FirebaseFirestore.instance.collection('books').snapshots();
-
   List<Book>? books;
 
-  void fetchBookList() {
-    _stream.listen((QuerySnapshot snapshot) {
-      final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
-        Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-        final String title = data['title'];
-        final String author = data['author'];
-        return Book(title, author);
-      }).toList();
-      this.books = books;
-      notifyListeners();
-    });
+  void fetchBookList() async {
+    // コレクションの指定
+    final QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('books').get();
+
+    // Firebase上のドキュメントの値(フィールド)をBookに変換する
+    final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
+      Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+      final String title = data['title'];
+      final String author = data['author'];
+      return Book(title, author);
+    }).toList();
+
+    this.books = books;
+    notifyListeners();
   }
 }
