062ae631906deaecff4154042f04d31ea3c23795
‘terupro’ <terupro911@gmail.com>
2022-05-14 14:45:05 +0900

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:book_list_sample/book_list/book_list_page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BookListPage(),
+    );
+  }
+}
